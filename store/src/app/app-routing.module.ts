b12f17f821fe1cf887072bbadd1fc6d7f761dcd7
claytoncc <clayton@openiot.co.za>
2020-08-21 10:19:25 +0200

    updates

--- store/src/app/app-routing.module.ts
+++ store/src/app/app-routing.module.ts
@@ -39,6 +39,14 @@ const routes: Routes = [
         'path':         'verify-account',
         'loadChildren': () => import('./pages/verify-account/verify-account.module').then(m => m.VerifyAccountModule)
     },
+    {
+        'path':         'privacy-policy',
+        'loadChildren': () => import('./pages/privacy-policy/privacy-policy.module').then(m => m.PrivacyPolicyModule)
+    },
+    {
+        'path':         'terms-and-conditions',
+        'loadChildren': () => import('./pages/terms-and-conditions/terms-and-conditions.module').then(m => m.TermsAndConditionsModule)
+    },
 	{
 		'path': 		'**',
 		'redirectTo': 	'products'
